7aec8293da0e01fec1d4f57337ed72cb8cfb6a6c
se7envlad <se7envlad@gmail.com>
2021-04-13 02:52:52 -0400

    My shitty coding at it's best XD
    added character code

--- source/Character.hx
+++ source/Character.hx
@@ -265,7 +265,6 @@ class Character extends FlxSprite
 				playAnim('idle');
 
 				flipX = true;
-
 			case 'bf':
 				var tex = Paths.getSparrowAtlas('BOYFRIEND','shared');
 				frames = tex;
@@ -304,6 +303,45 @@ class Character extends FlxSprite
 				playAnim('idle');
 
 				flipX = true;
+
+				case 'bf-alt':
+					var tex = Paths.getSparrowAtlas('BOYFRIEND','shared');
+					frames = tex;
+					animation.addByPrefix('idle', 'BF idle dance', 24, false);
+					animation.addByPrefix('singUP', 'BF NOTE UP0', 24, false);
+					animation.addByPrefix('singLEFT', 'BF NOTE LEFT0', 24, false);
+					animation.addByPrefix('singRIGHT', 'BF NOTE RIGHT0', 24, false);
+					animation.addByPrefix('singDOWN', 'BF NOTE DOWN0', 24, false);
+					animation.addByPrefix('singUPmiss', 'BF NOTE UP MISS', 24, false);
+					animation.addByPrefix('singLEFTmiss', 'BF NOTE LEFT MISS', 24, false);
+					animation.addByPrefix('singRIGHTmiss', 'BF NOTE RIGHT MISS', 24, false);
+					animation.addByPrefix('singDOWNmiss', 'BF NOTE DOWN MISS', 24, false);
+					animation.addByPrefix('hey', 'BF HEY', 24, false);
+
+					animation.addByPrefix('firstDeath', "BF dies", 24, false);
+					animation.addByPrefix('deathLoop', "BF Dead Loop", 24, true);
+					animation.addByPrefix('deathConfirm', "BF Dead confirm", 24, false);
+
+					animation.addByPrefix('scared', 'BF idle shaking', 24);
+
+					addOffset('idle', -5);
+					addOffset("singUP", -29, 27);
+					addOffset("singRIGHT", -38, -7);
+					addOffset("singLEFT", 12, -6);
+					addOffset("singDOWN", -10, -50);
+					addOffset("singUPmiss", -29, 27);
+					addOffset("singRIGHTmiss", -30, 21);
+					addOffset("singLEFTmiss", 12, 24);
+					addOffset("singDOWNmiss", -11, -19);
+					addOffset("hey", 7, 4);
+					addOffset('firstDeath', 37, 11);
+					addOffset('deathLoop', 37, 5);
+					addOffset('deathConfirm', 37, 69);
+					addOffset('scared', -4);
+
+					playAnim('idle');
+
+					flipX = true;
 
 			case 'bf-christmas':
 				var tex = Paths.getSparrowAtlas('christmas/bfChristmas');
@@ -495,6 +533,178 @@ class Character extends FlxSprite
 				addOffset("singDOWN-alt", -30, -27);
 
 				playAnim('idle');
+
+			case 'ruv_sheet':
+				// RUV code
+				tex = Paths.getSparrowAtlas('Mid-Fight-Masses/ruv_sheet','shared');
+				frames = tex;
+				animation.addByPrefix('idle', 'RuvIdle', 24);
+				animation.addByPrefix('singUP', 'RuvUp', 24);
+				animation.addByPrefix('singRIGHT', 'RuvRight', 24);
+				animation.addByPrefix('singDOWN', 'RuvDown', 24);
+				animation.addByPrefix('singLEFT', 'RuvLeft', 24);
+
+				addOffset('idle');
+				addOffset("singUP", 0, 0);
+				addOffset("singRIGHT", 0, 0);
+				addOffset("singLEFT", 0, 0);
+				addOffset("singDOWN", 0, 0);
+
+				playAnim('idle');
+			case 'ruv':
+				// RUV code
+				tex = Paths.getSparrowAtlas('Mid-Fight-Masses/ruv_sheet','shared');
+				frames = tex;
+				animation.addByPrefix('idle', 'RuvIdle', 24);
+				animation.addByPrefix('singUP', 'RuvUp', 24);
+				animation.addByPrefix('singRIGHT', 'RuvRight', 24);
+				animation.addByPrefix('singDOWN', 'RuvDown', 24);
+				animation.addByPrefix('singLEFT', 'RuvLeft', 24);
+
+				addOffset('idle');
+				addOffset("singUP", 0, 0);
+				addOffset("singRIGHT", 0, 0);
+				addOffset("singLEFT", 0, 0);
+				addOffset("singDOWN", 0, 0);
+
+				playAnim('idle');
+			case 'sarvente_dark':
+				// SARVDARK code
+				tex = Paths.getSparrowAtlas('Mid-Fight-Masses/sarvente_dark','shared');
+				frames = tex;
+				animation.addByPrefix('idle', 'SarvDarkIdle', 24);
+				animation.addByPrefix('singUP', 'SarvDarkUp', 24);
+				animation.addByPrefix('singRIGHT', 'SarvDarkRight', 24);
+				animation.addByPrefix('singDOWN', 'SarvDarkDown', 24);
+				animation.addByPrefix('singLEFT', 'SarvDarkLeft', 24);
+
+				addOffset('idle');
+				addOffset("singUP", 0, 0);
+				addOffset("singRIGHT", 0, 0);
+				addOffset("singLEFT", 0, 0);
+				addOffset("singDOWN", 0, 0);
+
+				playAnim('idle');
+			case 'sarvente-lucifer':
+				// SARV code
+				tex = Paths.getSparrowAtlas('Mid-Fight-Masses/smokinhotbabe','shared');
+				frames = tex;
+				animation.addByPrefix('idle', 'LuciferSarvIdle', 24);
+				animation.addByPrefix('singUP', 'LuciferSarvUp', 24);
+				animation.addByPrefix('singRIGHT', 'LuciferSarvRight', 24);
+				animation.addByPrefix('singDOWN', 'LuciferSarvDown', 24);
+				animation.addByPrefix('singLEFT', 'LuciferSarvLeft', 24);
+
+				addOffset('idle');
+				addOffset("singUP", 0, 0);
+				addOffset("singRIGHT", 0, 0);
+				addOffset("singLEFT", 0, 0);
+				addOffset("singDOWN", 0, 0);
+
+				playAnim('idle');
+			case 'sarvente-dark':
+				// SARVDARK code
+				tex = Paths.getSparrowAtlas('Mid-Fight-Masses/sarvente_dark','shared');
+				frames = tex;
+				animation.addByPrefix('idle', 'SarvDarkIdle', 24);
+				animation.addByPrefix('singUP', 'SarvDarkUp', 24);
+				animation.addByPrefix('singRIGHT', 'SarvDarkRight', 24);
+				animation.addByPrefix('singDOWN', 'SarvDarkDown', 24);
+				animation.addByPrefix('singLEFT', 'SarvDarkLeft', 24);
+
+				addOffset('idle');
+				addOffset("singUP", 0, 0);
+				addOffset("singRIGHT", 0, 0);
+				addOffset("singLEFT", 0, 0);
+				addOffset("singDOWN", 0, 0);
+
+				playAnim('idle');
+			case 'sarvente_sheet':
+				// SARV code
+				tex = Paths.getSparrowAtlas('Mid-Fight-Masses/sarvente_sheet','shared');
+				frames = tex;
+				animation.addByPrefix('idle', 'SarventeIdle', 24);
+				animation.addByPrefix('singUP', 'SarventeUp', 24);
+				animation.addByPrefix('singRIGHT', 'SarventeRight', 24);
+				animation.addByPrefix('singDOWN', 'SarventeDown', 24);
+				animation.addByPrefix('singLEFT', 'SarventeLeft', 24);
+
+				addOffset('idle');
+				addOffset("singUP", 0, 0);
+				addOffset("singRIGHT", 0, 0);
+				addOffset("singLEFT", 0, 0);
+				addOffset("singDOWN", 0, 0);
+
+				playAnim('idle');
+			case 'sarvente-sheet':
+				// SARV code
+				tex = Paths.getSparrowAtlas('Mid-Fight-Masses/sarvente_sheet','shared');
+				frames = tex;
+				animation.addByPrefix('idle', 'SarventeIdle', 24);
+				animation.addByPrefix('singUP', 'SarventeUp', 24);
+				animation.addByPrefix('singRIGHT', 'SarventeRight', 24);
+				animation.addByPrefix('singDOWN', 'SarventeDown', 24);
+				animation.addByPrefix('singLEFT', 'SarventeLeft', 24);
+
+				addOffset('idle');
+				addOffset("singUP", 0, 0);
+				addOffset("singRIGHT", 0, 0);
+				addOffset("singLEFT", 0, 0);
+				addOffset("singDOWN", 0, 0);
+
+				playAnim('idle');
+			case 'sarvente':
+				// SARV code
+				tex = Paths.getSparrowAtlas('Mid-Fight-Masses/sarvente_sheet','shared');
+				frames = tex;
+				animation.addByPrefix('idle', 'SarventeIdle', 24);
+				animation.addByPrefix('singUP', 'SarventeUp', 24);
+				animation.addByPrefix('singRIGHT', 'SarventeRight', 24);
+				animation.addByPrefix('singDOWN', 'SarventeDown', 24);
+				animation.addByPrefix('singLEFT', 'SarventeLeft', 24);
+
+				addOffset('idle');
+				addOffset("singUP", 0, 0);
+				addOffset("singRIGHT", 0, 0);
+				addOffset("singLEFT", 0, 0);
+				addOffset("singDOWN", 0, 0);
+
+				playAnim('idle');
+			case 'zardy':
+				// ZARDY code
+				tex = Paths.getSparrowAtlas('Zardy/Zardy','shared');
+				frames = tex;
+				animation.addByPrefix('idle', 'Idle', 24);
+				animation.addByPrefix('singUP', 'Sing Up', 24);
+				animation.addByPrefix('singRIGHT', 'Sing Right', 24);
+				animation.addByPrefix('singDOWN', 'Sing Down', 24);
+				animation.addByPrefix('singLEFT', 'Sing Left', 24);
+
+				addOffset('idle');
+				addOffset("singUP", 50, 0);
+				addOffset("singRIGHT", 50, 0);
+				addOffset("singLEFT", 50, 0);
+				addOffset("singDOWN", 50, 0);
+
+				playAnim('idle');
+
+				case 'sarvente_sheet-bf':
+					// SARV code
+					tex = Paths.getSparrowAtlas('Mid-Fight-Masses/sarvente_sheet','shared');
+					frames = tex;
+					animation.addByPrefix('idle', 'SarventeIdle', 24);
+					animation.addByPrefix('singUP', 'SarventeUp', 24);
+					animation.addByPrefix('singRIGHT', 'SarventeRight', 24);
+					animation.addByPrefix('singDOWN', 'SarventeDown', 24);
+					animation.addByPrefix('singLEFT', 'SarventeLeft', 24);
+
+					addOffset('idle', 0, 360);
+					addOffset("singUP", 0, 360);
+					addOffset("singRIGHT", 0, 360);
+					addOffset("singLEFT", 0, 360);
+					addOffset("singDOWN", 0, 360);
+
+					playAnim('idle');
 		}
 
 		dance();
